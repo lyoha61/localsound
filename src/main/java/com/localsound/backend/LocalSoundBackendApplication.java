135499c7b5bbf2f5c9550eeea9e2e77108f3090c
lyoha61 <nightmessenger703@gmail.com>
2026-08-14 02:51:37 +0300

    Initial commit

---
+++ src/main/java/com/localsound/backend/LocalSoundBackendApplication.java
@@ -0,0 +1,13 @@
+package com.localsound.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalSoundBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalSoundBackendApplication.class, args);
+	}
+
+}
